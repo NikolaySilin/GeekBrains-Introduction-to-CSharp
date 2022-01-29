5ec1aa089d4bd8d66b18b4ed3241fa99a3da34f7
Nikolay Silin <silinnikolay@yandex.ru>
2022-01-29 16:39:25 +0500

    add random

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -1,5 +1,7 @@
-﻿int _numberA = 3;
-int _numberB = 4;
+﻿int _numberA = new Random().Next(1, 123);
+Console.WriteLine("Random generated A: " + _numberA);
+int _numberB = new Random().Next(1, 321);
+Console.WriteLine("Random generated B: " + _numberB);
 int result = _numberA + _numberB;
 
-Console.WriteLine(result);
+Console.WriteLine("Summ A + B: " + result);
