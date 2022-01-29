425353c3ac0b74864e523986af181d714394d69f
Nikolay Silin <silinnikolay@yandex.ru>
2022-01-29 17:59:57 +0500

    add MagicCycle

---
+++ MagicСycle/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+
+int _xa = 40, _ya = 1;
+
+int _xb = 1, _yb = 30,
+    _xc = 80, _yc = 30;
+
+Console.SetCursorPosition(_xa, _ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(_xb, _yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(_xc, _yc);
+Console.WriteLine("+");
+
+int x = _xa, y = _xb;
+int count = 0;
+
+while(count < 10000)
+{
+    int what = new Random().Next(0, 3);
+    if(what == 0)
+    {
+        x = (x + _xa)/2;
+        y = (y + _ya)/2;
+    }
+
+    if(what == 1)
+    {
+        x = (x + _xb)/2;
+        y = (y + _yb)/2;
+    }
+
+    if(what == 2)
+    {
+        x = (x + _xc)/2;
+        y = (y + _yc)/2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
