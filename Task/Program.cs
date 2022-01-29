5605777b78b6b16abf987480df010ed218667757
Nikolay Silin <silinnikolay@yandex.ru>
2022-01-29 17:36:58 +0500

    add task

---
+++ Task/Program.cs
@@ -0,0 +1,15 @@
+﻿int _a = 1;
+int _b = 2;
+int _c = 6;
+int _d = 8;
+int _e = 4;
+
+int max = _a;
+
+if(_a > max) max = _a;
+if(_b > max) max = _b;
+if(_c > max) max = _c;
+if(_d > max) max = _d;
+if(_e > max) max = _e;
+
+Console.WriteLine("max = " + max);
