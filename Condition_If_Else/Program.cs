bcb0b550a4b08bf8fa0484e903546df418fbd8f8
Nikolay Silin <silinnikolay@yandex.ru>
2022-01-29 17:25:24 +0500

    add Condition If-Else

---
+++ Condition_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+
+string _username = Console.ReadLine();
+
+if (_username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же МАША!");
+}
+else
+{
+    Console.Write("Привет " + _username + "!");
+}
+  
