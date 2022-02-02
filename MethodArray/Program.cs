9e5c5241c844d7200d70697d84e689b5562dcbd2
Nikolay Silin <silinnikolay@yandex.ru>
2022-02-02 13:48:44 +0500

    Add MethodArray part 1

---
+++ MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {123, 12322, 233, 6654, 234, 8799, 65};
+
+int n = array.Length;
+
+int find = 233;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
