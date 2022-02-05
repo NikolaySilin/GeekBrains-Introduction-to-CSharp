0d65fbd50323413cfb7bd440dd6e270a81512eac
Nikolay Silin <silinnikolay@yandex.ru>
2022-02-05 12:30:12 +0500

    Task_4

---
+++ lesson_2/Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задать массив из 8 элементов и вывести их на экран.
+
+//int[] a = new int [8];
+
+int[] a = {1, 2, 3, 4, 5, 6, 7, 8};
+
+double[] b = new double[8];
+
+System.Console.WriteLine(a[0]);
+System.Console.WriteLine(a[7]);
+
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = new Random().Next(0, 100);
+}
+
+for (int i = 0; i < a.Length; i++)
+{
+    System.Console.Write($"{a[i]} ");
+}
