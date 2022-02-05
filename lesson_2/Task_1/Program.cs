5bbafbec535562691180e80ed671e54ca071faa9
Nikolay Silin <silinnikolay@yandex.ru>
2022-02-05 11:53:48 +0500

    Task_1

---
+++ lesson_2/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 5, b = 5;
+
+if (a > b) 
+{
+    System.Console.WriteLine("A больше Б");
+}
+if (a < b)
+{
+    System.Console.WriteLine("A не больше B");
+}
+if (a == b)
+{
+    System.Console.WriteLine("A равно B");
+}
