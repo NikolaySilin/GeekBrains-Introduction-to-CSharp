2ac314501c95a4cf07146a2c39e5c5b13d595064
Nikolay Silin <silinnikolay@yandex.ru>
2022-02-05 11:59:49 +0500

    Task_2

---
+++ lesson_2/Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти максимальное число из трех чисел.
+
+int a = 2, b = 5, c = 9;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+System.Console.WriteLine("Максимальное число: " + max);
