bbdbb5788aa4b5e54f1bb19d960eb03df3c6e6aa
Nikolay Silin <silinnikolay@yandex.ru>
2022-02-05 12:18:37 +0500

    Task_3

---
+++ lesson_2/Task_3/Program.cs
@@ -0,0 +1,30 @@
+﻿
+void Pause (string message) // описание подпрограммы (метода)
+{
+    System.Console.WriteLine(message);
+    Console.ReadKey();
+}
+
+Pause("Press any Key..."); //вызов метода
+
+
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+System.Console.WriteLine(Sum(5, 6)); 
+
+bool OddOrEven(int a)
+{
+    if (a % 2 ==0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+System.Console.WriteLine(OddOrEven(15));
