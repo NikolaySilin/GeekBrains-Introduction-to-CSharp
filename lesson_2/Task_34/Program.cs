bf9b3e5ab5761def65b0421996e98a4f53926215
Nikolay Silin <silinnikolay@yandex.ru>
2022-02-05 13:19:17 +0500

    Task_34

---
+++ lesson_2/Task_34/Program.cs
@@ -0,0 +1,22 @@
+﻿//Написать программу замену элементов массива на противоположные
+
+int[] array = new int[15];
+
+int number;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 100);
+
+    System.Console.Write(" " + array[i]);
+}
+
+System.Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]  *= -1;
+
+    System.Console.Write(" " + array[i]);
+}
+
