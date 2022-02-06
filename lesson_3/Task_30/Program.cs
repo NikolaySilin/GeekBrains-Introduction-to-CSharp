3769400902e9e1337e420a369913913e1f4cadec
Nikolay Silin <silinnikolay@yandex.ru>
2022-02-06 13:10:02 +0500

    Task_30

---
+++ lesson_3/Task_30/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+int[] array = new int[10];
+//int[] arr = {1, 2, 3, 4, 5};
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 100);
+
+    if (array[i] % 2 == 0)
+    {
+            System.Console.WriteLine($"{array[i]} {Math.Pow(array[i],3)}");
+    }
+}
+
+
+
