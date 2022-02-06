241a54dcb81b625ea2d1e949b2da20c53b04f3f1
Nikolay Silin <silinnikolay@yandex.ru>
2022-02-06 12:41:17 +0500

    Task_String_1

---
+++ lesson_3/Task_6/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу вычисления функции y = f(a)
+
+double F(double a)
+{
+    return a + a;
+}
+
+System.Console.WriteLine(F(2));
---
+++ lesson_3/Task_String_1/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string s = Console.ReadLine();
+string s1 = String.Empty;
+
+/* Вывод строки в консоль
+for (int i = 0; i < s.Length; i++)
+{
+    System.Console.Write(s[i]);
+}
+*/
+
+
+// Вывод перевернутой строки в консоль
+for (int i = s.Length - 1; i >= 0; i--)
+{
+    s1 = s1 + s[i];
+}
+System.Console.WriteLine(s1);
