31f061fc1a3fda50dd04f1cc9fc11bfbc0c05f88
Nikolay Silin <silinnikolay@yandex.ru>
2022-02-06 20:32:45 +0500

    Task_37

---
+++ lesson_2/Task_37/Program.cs
@@ -0,0 +1,34 @@
+﻿//В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] array = new int[123];
+
+int firstPoint = 10;
+int secondPoint = 99;
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(1, 150);
+    }
+}
+
+void findCountInInterval(int[] arr, int fPoint, int sPoint)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i >= fPoint && i <= sPoint)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine(count);
+}
+
+FillArray(array);
+
+findCountInInterval(array, firstPoint, secondPoint);
+
+
