acbc8393c67a52659cc31111bf08c9ccdfcac081
Nikolay Silin <silinnikolay@yandex.ru>
2022-02-18 22:24:53 +0500

    lesson 5

---
+++ lesson_5/Task_41/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить являются ли три числа сторонами треугольника
+
+double a = 10, b = 4, c = 7;
+
+bool IsTreug(double x, double y, double z)
+{
+    bool flag;
+    flag = (a + b > c && a + c > b && b + c > a);
+    return flag;
+}
+
+System.Console.WriteLine($"{a} {b} {c} - " + IsTreug(a, b, c));
+gi
