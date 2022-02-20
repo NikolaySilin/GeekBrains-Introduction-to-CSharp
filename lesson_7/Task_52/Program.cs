42522194b9f7948498a6a12ab1c96371d618dd49
Nikolay Silin <silinnikolay@yandex.ru>
2022-02-20 13:21:24 +0500

    Task_52

---
+++ lesson_7/Task_52/Program.cs
@@ -0,0 +1,60 @@
+﻿// В двумерном массиве заменить элементы, у которых оба индекса чётные на их квадраты
+
+int x, y;
+
+int[,] arr;
+
+void Input(out int x, out int y)
+{
+    System.Console.Write("Введите количество строк: ");
+    x = int.Parse(Console.ReadLine());
+
+    System.Console.Write("Введите количество столбцов: ");
+    y = int.Parse(Console.ReadLine());
+}
+
+void FillArray(int x, int y)
+{
+    arr = new int[x, y];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(-100,100);
+        }
+    }
+}
+
+void Solve(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {   
+            if(array[i, j] % 2 == 0)
+            {
+                array[i, j] = (int) Math.Pow(array[i, j], 2);
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j],7} |");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Input(out x, out y);
+FillArray(x, y);
+PrintArray(arr);
+System.Console.WriteLine();
+Solve(arr);
+PrintArray(arr);
