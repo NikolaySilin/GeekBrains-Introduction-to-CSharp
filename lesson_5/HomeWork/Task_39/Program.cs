c1141d1099af50d3f4a7ced6659ae017bfa4e1f4
Nikolay Silin <silinnikolay@yandex.ru>
2022-02-21 19:49:53 +0500

    Home work Task 39 - 40

---
+++ lesson_5/HomeWork/Task_39/Program.cs
@@ -0,0 +1,53 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+int[] array;
+
+int sizeArray;
+
+int x = 0, y = 0;
+
+void Input(out int sizeArr)
+{
+    System.Console.Write("Введите размер массива: ");
+    sizeArr = int.Parse(Console.ReadLine());
+}
+
+void FillArray(int sizeArr)
+{
+    array = new int[sizeArr];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i],5}   |");
+    }
+}
+
+void Solution(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i == 0)
+        {
+            x = arr[i];
+        }
+         
+         y = arr[i];
+    }
+    System.Console.WriteLine($"\nПервое: {x}, и последнее: {y}, число в массиве");
+    System.Console.WriteLine($"\nИх произведение равно: {x * y}\n");
+}
+
+Input(out sizeArray);
+FillArray(sizeArray);
+System.Console.WriteLine();
+PrintArray(array);
+System.Console.WriteLine();
+Solution(array);
---
+++ lesson_5/HomeWork/Task_40/Program.cs
@@ -0,0 +1,62 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+
+double[] array;
+
+double max = 0, min = 0;
+int sizeArray = 0;
+
+void Input(out int sizeArr)
+{
+    System.Console.Write($"Введите размер массива: ");
+    sizeArr = int.Parse(Console.ReadLine());
+}
+
+void FillArray(int sizeArr)
+{
+    array = new double[sizeArr];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().NextDouble() * 100;
+    }
+}
+
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i],8:F2}  |");
+    }
+}
+
+void Solution(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i == 0)
+        {
+            min = arr[i];
+            max = arr[i];
+        }
+
+        if(min >= arr[i])
+        {
+            min = arr[i];
+        }
+
+        if(max <= arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    System.Console.WriteLine($"Наименьшее число в массиве: {min,5:F2}");
+    System.Console.WriteLine($"Наибольшее число в массиве: {max,5:F2}");
+    System.Console.WriteLine($"Разница между максимальным и минимальным значением в массиве: {max - min:F2}");
+}
+
+Input(out sizeArray);
+FillArray(sizeArray);
+PrintArray(array);
+System.Console.WriteLine();
+Solution(array);
+
