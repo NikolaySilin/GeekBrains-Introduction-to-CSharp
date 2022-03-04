e4234d1f0b1e7102c4f9ec4481c16d9985d338f4
Nikolay Silin <silinnikolay@yandex.ru>
2022-03-04 20:35:08 +0500

    Task 56

---
+++ lesson_7/HomeWork/Task_56/Program.cs
@@ -0,0 +1,59 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+
+int x = 0, y = 0;
+
+int temporary = 0;
+
+int[,] array;
+
+void Input(out int x, out int y)
+{
+    System.Console.Write("Введите количество строк: ");
+    x = int.Parse(Console.ReadLine());
+
+    System.Console.Write("Введите количество столбцов: ");
+    y = int.Parse(Console.ReadLine());
+}
+
+void FillArray(int x, int y)
+{
+    array = new int[x, y];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+    }
+}
+
+void Solution(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        temporary = arr[0, i];
+        arr[0, i] = arr[arr.GetLength(0) - 1, i];
+        arr[arr.GetLength(0) - 1, i] = temporary;
+    }
+}   
+
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j],7} |");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Input(out x, out y);
+FillArray(x, y);
+PrintArray(array);
+Solution(array);
+System.Console.WriteLine();
+PrintArray(array);
