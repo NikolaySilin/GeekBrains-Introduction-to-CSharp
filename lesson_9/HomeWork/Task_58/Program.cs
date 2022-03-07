0c692afa69c190e2985d59c9bbd886b7b64495c0
Nikolay Silin <silinnikolay@yandex.ru>
2022-03-07 22:06:02 +0500

    HomeWork Task 58

---
+++ lesson_9/HomeWork/Task_58/Program.cs
@@ -0,0 +1,60 @@
+﻿// Написать программу, которая в двумерном массиве заменяет строки на столбцы или сообщить, 
+// что это невозможно (в случае, если матрица не квадратная).
+
+int rows, columns;
+int[,] array = null;
+int[,] temporaryArray = null;
+
+void Input(out int rows, out int columns)
+{
+    System.Console.Write("Введите количество строк: ");
+    rows = int.Parse(Console.ReadLine());
+
+    System.Console.Write("Введите количество столбцов: ");
+    columns = int.Parse(Console.ReadLine());
+}
+
+void FillArray(out int[,] arr, int rows, int columns)
+{
+    arr = new int[rows, columns];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void Solution(int[,] arr, ref int[,] tempArr)
+{
+    tempArr = new int[arr.GetLength(0), arr.GetLength(1)];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            tempArr[i, j] = arr[j, i];
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j],3} |");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+Input(out rows, out columns);
+FillArray(out array, rows, columns);
+PrintArray(array);
+System.Console.WriteLine();
+Solution(array, ref temporaryArray);
+PrintArray(temporaryArray);
---
+++ lesson_9/Task_61/Program.cs
@@ -0,0 +1,52 @@
+﻿// Найти произведение двух матриц
+
+int[,] arrayOne = new int[5, 5];
+int[,] arrayTwo = new int[5, 5];
+int[,] arrayResult = new int[5, 5];
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            System.Console.Write($"{arr[i, j],5}  |");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void Solution(int[,] firstArr, int[,] secondArr, int[,] arrResult)
+{
+    for (int i = 0; i < firstArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < firstArr.GetLength(1); j++)
+        {
+            arrResult[i, j] = firstArr[i, j] * secondArr[i, j];
+        }
+    }
+}
+
+FillArray(arrayOne);
+FillArray(arrayTwo);
+
+PrintArray(arrayOne);
+System.Console.WriteLine();
+PrintArray(arrayTwo);
+
+Solution(arrayOne, arrayTwo, arrayResult);
+
+System.Console.WriteLine();
+
+PrintArray(arrayResult);
